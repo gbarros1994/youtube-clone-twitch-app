270838eda5ab5269468dc15ed00e84a5f96272ea
gerson <gerson.barros@m2n.com.br>
2020-11-19 17:04:49 -0300

    rotas e icones no menu

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Following: React.FC = () => {
+  return (
+    <Container>
+      <Text>ComingSoon</Text>
+    </Container>
+  )
+};
+
+export default Following;
