4cdb48edd315c942cb0e24e60b04428b63a39670
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2020-11-28 20:44:17 -0300

    Categorias

---
+++ src/components/CategoryList/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const List = styled.ScrollView.attrs({
+    horizontal: true,
+})``;
+export const CategoryContainer = styled.TouchableOpacity``;
+export const CategoryImage = styled.Image``;
+export const CategoryName = styled.Text``;
+export const CategoryStatus = styled.View``;
+export const RedCircle = styled.View``;
+export const Info = styled.Text``;
